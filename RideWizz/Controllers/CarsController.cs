13e116dbf7233830c32e4e1c01fac2b15b1f9837
Momin Khan <foolmoron@gmail.com>
2015-12-29 19:12:24 -0500

    Added view output model and bad logo

--- RideWizz/Controllers/CarsController.cs
+++ RideWizz/Controllers/CarsController.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Data.Repositories;
+using RideWizz.Models;
 
 namespace RideWizz.Controllers {
     public class CarsController : Controller {
 
         public ActionResult Index() {
-            var cars = new CarRepository().GetAll();
-            return View();
+            var model = new CarsOutputModel {
+                AllCars = new CarRepository().GetAll()
+            };
+            return View(model);
         }
     }
 }
---
+++ RideWizz/Models/CarsOutputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data.Models;
+
+namespace RideWizz.Models {
+    public class CarsOutputModel {
+        public IEnumerable<Car> AllCars;
+    }
+}
